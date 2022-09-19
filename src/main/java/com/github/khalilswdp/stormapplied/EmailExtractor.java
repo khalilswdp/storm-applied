5cf117bb56398b0e7c1cfe48f6dd09cf04270140
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-09-19 18:56:34 +0100

    Use Slf4j for logging

--- src/main/java/com/github/khalilswdp/stormapplied/EmailExtractor.java
+++ src/main/java/com/github/khalilswdp/stormapplied/EmailExtractor.java
@@ -1,5 +1,6 @@
 package com.github.khalilswdp.stormapplied;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.storm.topology.BasicOutputCollector;
 import org.apache.storm.topology.OutputFieldsDeclarer;
 import org.apache.storm.topology.base.BaseBasicBolt;
@@ -7,6 +8,7 @@ import org.apache.storm.tuple.Fields;
 import org.apache.storm.tuple.Tuple;
 import org.apache.storm.tuple.Values;
 
+@Slf4j
 public class EmailExtractor extends BaseBasicBolt {
 
     @Override
